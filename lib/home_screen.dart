5f70dd570bd6436bf8f9c799a9a8be642e3d4297
JDevshington <202010314@itla.edu.do>
2023-07-19 12:08:55 -0400

    TODO App - Third Commit(Top bar creation)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,29 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text(
+            "DP-TODO",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.help_outline_rounded,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
