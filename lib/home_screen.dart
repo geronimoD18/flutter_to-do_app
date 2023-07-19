b87f4a91f562c4af85e96ae21dad959140dd3a5c
JDevshington <202010314@itla.edu.do>
2023-07-19 14:12:42 -0400

    TODO App - Fifth Commit(Registry deletion implemented)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -66,17 +66,98 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
               );
             }
             else{
-              return Container();
-              // return ListView.builder(
-              //   shrinkWrap: true,
-              //   itemCount: snapshot,
-              // );
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index){
+                  int todoId = snapshot.data![index].id!.toInt();
+                  String todoTitle = snapshot.data![index].title.toString();
+                  String todoDesc = snapshot.data![index].desc.toString();
+                  String todoDT = snapshot.data![index].dateandtime.toString();
+                  return Dismissible(
+                    key: ValueKey<int>(todoId),
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      color: Colors.redAccent,
+                      child: Icon(Icons.delete_forever, color: Colors.white,),
+                    ),
+                    onDismissed: (DismissDirection direction){
+                      setState(() {
+                        dbHelper!.delete(todoId);
+                        dataList = dbHelper!.getDataList();
+                        snapshot.data!.remove(snapshot.data![index]);
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow.shade300,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            contentPadding: EdgeInsets.all(10),
+                            title: Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                  todoTitle,
+                                style: TextStyle(fontSize: 19),
+                              ),
+                            ),
+                            subtitle: Text(
+                              todoDesc,
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.black,
+                            thickness: 0.8,
+                          ),
+                          Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 3, horizontal: 10),
+                            child: Row(
+                              mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                todoDT,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: Icon(
+                                  Icons.edit_note,
+                                  size: 28,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
             }
           },
         )),
